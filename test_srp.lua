11dd6c4e24bc7eb75c4875e80d20ea046425c4a9
Chen Tao <cantoo523@gmail.com>
2016-03-08 14:17:01 +0800

    Update test_srp.lua

--- test_srp.lua
+++ test_srp.lua
@@ -1,34 +1,42 @@
 local srp = require ("srp")
-local g, N = srp.get_default_gN();
-print("g: " .. g);
-print("N: " .. N);
 
 local username = "username"
 local passwd = "passwd"
-local s, v = srp.create_verifier(username, passwd, N, g)
-print("s: " .. s)
-print("v: " .. v)
+print("Client ============== > Server:\n"
+	.. "username = " .. username .. "\n")
 
-local b = srp.RAND_pseudo_bytes(32)
-print("b: " .. b)
-local B = srp.Calc_B(b, N, g, v)
-print("B: " .. B)
-print("B mod N: " .. srp.Verify_mod_N(B, N))
+local g, N = srp.get_default_gN();
+local s, v = srp.create_verifier(username, passwd, N, g)
+print("Server ============== > Client:\n"
+	.. "N = " .. N .. "\n"
+	.. "g = " .. g .. "\n"
+	.. "s = " .. s .. "\n")
 
 local a = srp.RAND_pseudo_bytes(32)
-print("a: " .. a)
+--print("Client a: " .. a)
 local A = srp.Calc_A(a, N, g)
-print("A: " .. A)
-print("A mod N: " .. srp.Verify_mod_N(A, N))
+print("Client ============== > Server:\n"
+	.. "A = " .. A .. "\n")
 
+print("Server verify A mod N: " .. srp.Verify_mod_N(A, N))
+local b = srp.RAND_pseudo_bytes(32)
+--print("b: " .. b)
+local B = srp.Calc_B(b, N, g, v)
+print("Server ============== > Client:\n"
+	.. "B = " .. B .. "\n")
+	
+print("Client verify B mod N: " .. srp.Verify_mod_N(B, N))
 local Kclient = srp.Calc_client_key(A, B, N, s, username, passwd, g, a)
-print("Kclient: " .. Kclient);
-
-local Kserver = srp.Calc_server_key(A, B, N, v, b);
-print("Kserver: " .. Kserver);
-
+--print("Kclient: " .. Kclient);
 local M1 = srp.Calc_M1(N, g, username, s, A, B, Kclient);
-print("M1: " .. M1)
+print("Client ============== > Server:\n"
+	.. "M1 = " .. M1 .. "\n")
 
+print("Server verify M1 match")
+local Kserver = srp.Calc_server_key(A, B, N, v, b);
+--print("Kserver: " .. Kserver);
 local M2 = srp.Calc_M2(A, M1, Kserver)
-print("M2: " .. M2)
+print("Server ============== > Client:\n"
+	.. "M2 = " .. M2 .. "\n")
+
+print("Client verify M2 match \n")
